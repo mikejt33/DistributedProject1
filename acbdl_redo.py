19209cb3c31b67b132150bfe8c3a7beaa4319682
root <root@Surface.localdomain>
2018-03-23 18:04:59 -0400

    acbdl_redo.py for grabbing date,article,#edits with quotes around article titles from enwiki-blahblah.main.bz2

---
+++ acbdl_redo.py
@@ -0,0 +1,36 @@
+"""Get article counts by day.
+
+Creates a three column file with date, articletitle, count. For cleaned up
+version of data.
+
+Author: Joe
+"""
+
+from mrjob.job import MRJob
+
+
+class articlecounts(MRJob):
+
+    def mapper(self, _, line):
+        splt = row.split()
+        if len(splt) > 1 and splt[0] == "REVISION":
+            if splt[4][:3] == "200":
+                key = splt[4]  # The date of the edit is the key
+                key = key[:10]  # Cut off time stamp
+                article = "\"" + splt[3] + "\""
+                yield(key, article)
+
+    def reducer(self, key, vals):
+        counts = {}
+        for v in vals:
+            try:
+                counts[str(v)] += 1
+            except:
+                counts[str(v)] = 1
+        for entry in counts:
+            if counts[entry] > 0:
+                out_string = str(key) + "," + str(entry) + "," + str(counts[entry])
+                print(out_string)
+
+if __name__ == '__main__':
+    articlecounts.run()
