d5f5e2fa1d0761c0cd404ffa9bec796c065403a7
josephcomer <joseph.comer@ncf.edu>
2018-03-24 12:33:37 -0400

    Throwing literally everything into a try clause to see if we can just skip the broken lines.

--- acbdl_redo.py
+++ acbdl_redo.py
@@ -14,23 +14,29 @@ class articlecounts(MRJob):
     def mapper(self, _, line):
         splt = line.split()
         if len(splt) > 1 and splt[0] == "REVISION":
-            if splt[4][:3] == "200":
-                key = splt[4]  # The date of the edit is the key
-                key = key[:10]  # Cut off time stamp
-                article = "\"" + splt[3] + "\""
-                yield(key, article)
+            try:
+                if splt[4][:3] == "200":
+                    key = splt[4]  # The date of the edit is the key
+                    key = key[:10]  # Cut off time stamp
+                    article = "\"" + splt[3] + "\""
+                    yield(key, article)
+            except:
+                pass
 
     def reducer(self, key, vals):
-        counts = {}
-        for v in vals:
-            try:
-                counts[str(v)] += 1
-            except:
-                counts[str(v)] = 1
-        for entry in counts:
-            if counts[entry] > 0:
-                out_string = str(key) + "," + str(entry) + "," + str(counts[entry])
-                print(out_string)
+        try:
+            counts = {}
+            for v in vals:
+                try:
+                    counts[str(v)] += 1
+                except:
+                    counts[str(v)] = 1
+            for entry in counts:
+                if counts[entry] > 0:
+                    out_string = str(key) + "," + str(entry) + "," + str(counts[entry])
+                    print(out_string)
+        except:
+            pass
 
 if __name__ == '__main__':
     articlecounts.run()
